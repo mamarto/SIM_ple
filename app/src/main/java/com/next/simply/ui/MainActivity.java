88137087c9ad2259448c1230f1c60a31b87bab65
Agostinelli <martoranamanfredi@gmail.com>
2015-08-15 12:42:13 +0200

    splash activity added

--- app/src/main/java/com/next/simply/ui/MainActivity.java
+++ app/src/main/java/com/next/simply/ui/MainActivity.java
@@ -1,6 +1,5 @@
 package com.next.simply.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,16 +8,17 @@ import com.next.simply.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private android.support.v7.app.ActionBar mActionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, ContactsActivity.class);
-        startActivity(intent);
+
     }
 
-//    @Override
+    //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
 //        getMenuInflater().inflate(R.menu.menu_main, menu);
